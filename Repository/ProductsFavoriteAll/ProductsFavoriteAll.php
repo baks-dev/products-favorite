85445155f70b640fb2075554289ee318c55bab9d
adminap <shestopalov92@gmail.com>
2025-03-21 11:15:58 +0300

    fork: pre-release 7.2.4

--- Repository/ProductsFavoriteAll/ProductsFavoriteAll.php
+++ Repository/ProductsFavoriteAll/ProductsFavoriteAll.php
@@ -110,6 +110,7 @@ final class ProductsFavoriteAll implements ProductsFavoriteAllInterface
 
         /** OFFER */
         $dbal
+            ->addSelect('product_offer.id AS product_offer_id')
             ->addSelect('product_offer.const AS product_offer_const')
             ->addSelect("product_offer.value as product_offer_value")
             ->leftJoin(
@@ -131,6 +132,7 @@ final class ProductsFavoriteAll implements ProductsFavoriteAllInterface
 
         /** VARIATION */
         $dbal
+            ->addSelect('product_variation.id AS product_variation_id')
             ->addSelect('product_variation.const AS product_variation_const')
             ->addSelect("product_variation.value as product_variation_value")
             ->leftJoin(
@@ -152,6 +154,7 @@ final class ProductsFavoriteAll implements ProductsFavoriteAllInterface
 
         /** MODIFICATION */
         $dbal
+            ->addSelect('product_modification.id AS product_modification_id')
             ->addSelect('product_modification.const AS product_modification_const')
             ->addSelect("product_modification.value as product_modification_value")
             ->leftJoin(
--- Twig/RenderFavoriteButtonExtension.php
+++ Twig/RenderFavoriteButtonExtension.php
@@ -1,4 +1,26 @@
 <?php
+/*
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ *
+ */
 
 namespace BaksDev\Products\Favorite\Twig;
 
@@ -60,8 +82,13 @@ class RenderFavoriteButtonExtension extends AbstractExtension
     /**
      * @example {{ render_favorite_button('2a1ea75d-c43a-7eb6-8236-71d3e3804671') }}
      */
-    public function renderButton(Environment $twig, string $invariable): string
+    public function renderButton(Environment $twig, string|null $invariable = null): string
     {
+        if(is_null($invariable))
+        {
+            return '';
+        }
+
         $isUsr = $this->userTokenStorage->isUser();
 
         $ProductsFavoriteDTO = new AnonymousProductsFavoriteDTO()
