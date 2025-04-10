5ca2b526742e01465f270c714d875fc289ab82d0
kingstar <admin@baks.dev>
2025-04-10 11:05:16 +0300

    Baks Development

--- Messenger/ProductsFavoriteMessage.php
+++ Messenger/ProductsFavoriteMessage.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace BaksDev\Products\Favorite\Messenger;
 
+use Symfony\Component\DependencyInjection\Attribute\Exclude;
+
+#[Exclude]
 final class ProductsFavoriteMessage
 {
     public function __construct()
